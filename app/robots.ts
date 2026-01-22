0c7152f2a4234d9e9409dfc7bf936d3f275b51ce
mdnematull <nematull69@gmail.com>
2026-01-22 10:27:13 +0530

    Fixed JSX syntax and motion.div errors

--- app/robots.ts
+++ app/robots.ts
@@ -8,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
         allow: "/",
       },
     ],
-    sitemap: "https://pdfswift.online/sitemap.xml",
+    sitemap: "https://www.pdfswift.online/sitemap.xml",
   };
 }
